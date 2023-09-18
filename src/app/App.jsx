22247b22308ffa839fa0d7ff958ccddaa1be2104
German Kosach <fuerstein@yandex.ru>
2023-09-18 21:03:29 +0300

    lets start

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className='step1-container'>
+        <h1 className='step-title'>Бронирование номера</h1>
+        <div className='step-content'>{/* Your form elements here */}</div>
+      </div>
+    </>
+  )
+}
+
+export default App
