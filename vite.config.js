8fdd67d0457d71f7e8f6ea28c8bf5fd7509822cd
German Kosach <fuerstein@yandex.ru>
2023-09-19 23:50:34 +0300

    s

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +11,11 @@ export default defineConfig({
     },
   },
 
-
+  build: {
+    rollupOptions: {
+      external: ['react-dom/client'],
+    },
+  },
   plugins: [react()],
 
 
